8c59b26cb4f41b82b09e0cd3bcd1d12ae76c6340
jkagubare20 <kagubarejesus@gmail.com>
2025-08-11 12:29:03 +0200

    Mindfulness program

--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -4,49 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Menu Options:");
-        Console.WriteLine("1. Breathing Activity");
-        Console.WriteLine("2. Reflecting Activity");
-        Console.WriteLine("3. Listing Activity");
-        Console.WriteLine("4. Exit");
-        Console.Write("Select an option (1-4): ");
-        string choice = Console.ReadLine();
-
-        if (choice == "1")
+        Console.WriteLine("Welcome to the Mindfulness Program!");
+        string choice;
+        Console.WriteLine("This program will guide you through various mindfulness activities to help you relax and reflect."); 
+        
+        do
         {
-            BreathingActivity breathingActivity = new BreathingActivity("Breathing Activity", "This activity will help you relax by guiding you through a series of deep breathing exercises.");
-            breathingActivity.DisplayStartingMessage();
-            breathingActivity.Run();
-            breathingActivity.DisplayEndingMessage();
-        }
-
-        else if (choice == "2")
-        {
-            ReflectingActivity reflectingActivity = new ReflectingActivity("Reflecting Activity", "This activity will help you reflect on times in your life when you have shown strength and resilience.");
-            reflectingActivity.DisplayStartingMessage();
-            reflectingActivity.Run();
-            reflectingActivity.DisplayEndingMessage();
-        }
-
-        else if (choice == "3")
-        {
-            ListingActivity listingActivity = new ListingActivity("Listing Activity", "This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area.");
-            listingActivity.DisplayStartingMessage();
-            listingActivity.Run();
-            listingActivity.GetListFromUser();
-            listingActivity.DisplayEndingMessage();
-        }
-
-        else if (choice == "4")
-        {
-            Console.WriteLine("Exiting the program. Goodbye!");
-        }
-
-        else
-        {
-            Console.WriteLine("Invalid choice. Please select a valid option.");
-        }
-
+            Console.WriteLine("Menu Options:");
+            Console.WriteLine("1. Breathing Activity");
+            Console.WriteLine("2. Reflecting Activity");
+            Console.WriteLine("3. Listing Activity");
+            Console.WriteLine("4. Exit");
+            Console.Write("Select an option (1-4): ");
+            choice = Console.ReadLine();
+            if (choice == "1")
+            {
+                BreathingActivity breathingActivity = new BreathingActivity("Breathing Activity", "This activity will help you relax by guiding you through a series of deep breathing exercises.");
+                breathingActivity.DisplayStartingMessage();
+                breathingActivity.Run();
+                breathingActivity.DisplayEndingMessage();
+            }
+            else if (choice == "2")
+            {
+                ReflectingActivity reflectingActivity = new ReflectingActivity("Reflecting Activity", "This activity will help you reflect on times in your life when you have shown strength and resilience.");
+                reflectingActivity.DisplayStartingMessage();
+                reflectingActivity.Run();
+                reflectingActivity.DisplayEndingMessage();
+            }
+            else if (choice == "3")
+            {
+                ListingActivity listingActivity = new ListingActivity("Listing Activity", "This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area.");
+                listingActivity.DisplayStartingMessage();
+                listingActivity.Run();
+                listingActivity.GetListFromUser();
+                listingActivity.DisplayEndingMessage();
+            }
+            else if (choice == "4")
+            {
+                Console.WriteLine("Exiting the program. Goodbye!");
+                break;
+            }
+        } while (choice != "4");
     }
 }
 
